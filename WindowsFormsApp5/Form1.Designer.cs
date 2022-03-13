d108ba7764cc0300e1afa1a2e5b45c0bdafaf6ae
QaraByte <pchelpkz16@gmail.com>
2022-03-13 16:49:35 +0600

    Add project files.

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnPutImg = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(212, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(868, 564);
+            this.panel2.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(868, 564);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(206, 50);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Подключение базы данных";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Подключить БД";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.btnPutImg);
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(212, 564);
+            this.panel1.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(3, 68);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 64);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Добавление и удаление данных";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(177, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Удалить строку";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Добавить строку";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnPutImg
+            // 
+            this.btnPutImg.Location = new System.Drawing.Point(9, 161);
+            this.btnPutImg.Name = "btnPutImg";
+            this.btnPutImg.Size = new System.Drawing.Size(177, 23);
+            this.btnPutImg.TabIndex = 6;
+            this.btnPutImg.Text = "Вставить картинку";
+            this.btnPutImg.UseVisualStyleBackColor = true;
+            this.btnPutImg.Click += new System.EventHandler(this.btnPutImg_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(30, 272);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(156, 89);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 564);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "База данных SQLite";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnPutImg;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.IO;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+
+        private SQLiteConnection SQLiteConn;
+        private DataTable dTable;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SQLiteConn = new SQLiteConnection();
+            dTable = new DataTable();
+        }
+        private bool OpenDBFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openFileDialog.Filter = "Текстовые файлы (*.sqlite)|*.sqlite| Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog(this)==DialogResult.OK)
+            {
+                SQLiteConn = new SQLiteConnection("Data Source =" + openFileDialog.FileName + ";Version = 3;");
+                SQLiteConn.Open();
+                SQLiteCommand command = new SQLiteCommand();
+                command.Connection = SQLiteConn;
+                return true;
+            }
+            else return false;
+        }
+        private void ShowTable()
+        {
+            string SQLQue;
+            string SQLQuery = "SELECT name FROM sqlite_master WHERE type = 'table' ORDER BY name;";
+            SQLiteCommand command = new SQLiteCommand(SQLQuery, SQLiteConn);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                SQLQue = "SELECT * FROM [" + reader[0].ToString() + "] order by 1";
+
+                dTable.Clear();
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(SQLQue, SQLiteConn);
+                adapter.Fill(dTable);
+
+                dataGridView1.Columns.Clear();
+                dataGridView1.Rows.Clear();
+
+                for (int col = 0; col < dTable.Columns.Count; col++)
+                {
+                    string ColName = dTable.Columns[col].ColumnName;
+                    dataGridView1.Columns.Add(ColName, ColName);
+                    dataGridView1.Columns[col].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                }
+                for (int row = 0; row < dTable.Rows.Count; row++)
+                {
+                    dataGridView1.Rows.Add(dTable.Rows[row].ItemArray);
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (OpenDBFile() == true)
+            {
+                ShowTable();
+            }
+        }
+        private double MaxValue()
+        {
+
+            double value1, value2,value,max=0;
+            int i = 0, j=1;
+            while (j< dTable.Rows.Count)
+            {
+                value1 = Convert.ToDouble(dTable.Rows[i].ItemArray[1]);
+                value2 = Convert.ToDouble(dTable.Rows[j].ItemArray[1]);
+                value = Math.Abs(value1 - value2);
+                if (value > max) { max = value; }
+                i++;
+                j = i + 1;
+            }
+            return max;
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            double rmax,rmin;
+            rmax = Convert.ToDouble(dTable.Rows[0].ItemArray[1])+MaxValue();
+            rmin = Convert.ToDouble(dTable.Rows[0].ItemArray[1]) - MaxValue();
+            Random random = new Random();
+            double r;
+            List<string> values = new List<string>();
+            string SQLQuery = "SELECT name FROM sqlite_master WHERE type = 'table' ORDER BY name;";
+            SQLiteCommand command = new SQLiteCommand(SQLQuery, SQLiteConn);
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            { string sqlBegin = "INSERT INTO ["+reader[0].ToString()+"] (";
+                string sqlBody = "";
+                for (int i = 0; i < dTable.Columns.Count; i++)
+                {
+                    r = random.NextDouble() * (rmax - rmin) + rmin;
+                    if (i == 0) values.Add((dTable.Rows.Count).ToString());
+                    else values.Add(Math.Round(r, 4).ToString());
+                    if (i != 0 && i >= dTable.Columns.Count - 1)
+                        sqlBody += "\"" + dTable.Columns[i].ColumnName + "\")";
+                    else sqlBody += "\"" + dTable.Columns[i].ColumnName + "\",";
+                }
+
+                try
+                {
+                    SQLiteCommand com = new SQLiteCommand(SQLiteConn);
+                    com.CommandText = sqlBegin + sqlBody + "VALUES (" + String.Join(",", values) + ")";
+                    com.ExecuteNonQuery();
+
+                    ShowTable();
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                } 
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string SQLQuery = "SELECT name FROM sqlite_master WHERE type = 'table' ORDER BY name;";
+            SQLiteCommand command = new SQLiteCommand(SQLQuery, SQLiteConn);
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                    SQLiteCommand sqlEnd = new SQLiteCommand(SQLiteConn);
+                    sqlEnd.CommandText = "DELETE  FROM [" + reader[0].ToString() + "] WHERE Эпоха  = \"" + (dTable.Rows.Count - 1).ToString() + "\";";
+                    sqlEnd.ExecuteNonQuery();
+                    ShowTable();
+
+            }
+        }
+
+        private void btnPutImg_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter= "jpg files (*.jpg)|*.jpg|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                string sql = "INSERT INTO [test] (img) values ('img\\camry10.jpg')";
+
+                try
+                {
+                    Image img = Image.FromFile(dlg.FileName);
+                    byte[] photo = ImageToByteArray(img);
+
+                    SQLiteCommand com = new SQLiteCommand(SQLiteConn);
+                    com.CommandText = "INSERT INTO test (img) VALUES (@photo)";
+                    com.Parameters.Add("@photo", DbType.Binary, 20).Value = photo;
+                    com.ExecuteNonQuery();
+
+
+                    string SQLQuery = "SELECT img FROM test WHERE id=1;";
+                    SQLiteCommand command = new SQLiteCommand(SQLQuery, SQLiteConn);
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            byte[] buffer = GetBytes(reader);
+
+                            using (var ms = new MemoryStream(buffer))
+                            {
+                                pictureBox1.Image =Image.FromStream(ms);
+                            }
+                        }
+                    }
+                    SQLiteConn.Close();
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        public byte[] ImageToByteArray(System.Drawing.Image imageIn)
+        {
+            using (var ms = new MemoryStream())
+            {
+                imageIn.Save(ms, imageIn.RawFormat);
+                return ms.ToArray();
+            }
+        }
+
+        static byte[] GetBytes(SQLiteDataReader reader)
+        {
+            const int CHUNK_SIZE = 2 * 1024;
+            byte[] buffer = new byte[CHUNK_SIZE];
+            long bytesRead;
+            long fieldOffset = 0;
+            using (MemoryStream stream = new MemoryStream())
+            {
+                while ((bytesRead = reader.GetBytes(0, fieldOffset, buffer, 0, buffer.Length)) > 0)
+                {
+                    stream.Write(buffer, 0, (int)bytesRead);
+                    fieldOffset += bytesRead;
+                }
+                return stream.ToArray();
+            }
+        }
+    }
+}
